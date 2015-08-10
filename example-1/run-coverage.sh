7202f76e3467a7d8a4376ce521901465f1794798
Jason Hinkle <jason@eventric.com>
2015-08-10 15:46:58 -0500

    add comments

--- example-1/run-coverage.sh
+++ example-1/run-coverage.sh
@@ -12,6 +12,7 @@ echo "Starting Selenium Server..."
 java -jar ../selenium/selenium-server-standalone-2.47.1.jar &> tests-output/selenium.log &
 sleep 1
 
+# read by phpunit_bootstrap.php (options: firefox or phantomjs)
 export UNIT_TEST_BROWSER=phantomjs
 
 phpunit --coverage-html ./tests-output/coverage tests/
